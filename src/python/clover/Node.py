10deca5b04b1bb6f62568706ec65cb43ee60a7fe
Maurice Bowen <mauricebeauxen@gmail.com>
2016-10-31 21:07:43 -0700

    build

---
+++ src/python/clover/Node.py
@@ -0,0 +1,5 @@
+class Node(object):
+    def __init__(self,item=None, left=None, right=None):
+        self.left = left
+        self.right = right
+        self.item = item
---
+++ src/python/clover/Queue.py
@@ -0,0 +1,3 @@
+class Queue(Node):
+    def __init__(self):
+        
